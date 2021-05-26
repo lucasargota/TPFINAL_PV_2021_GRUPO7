6643d928e360c10b1a7bb08b8e85785bbe9cfc37
Nano <Nano@DESKTOP-6LMQ8E6>
2021-05-26 01:26:29 -0300

    creando branch

--- src/main/java/ar/edu/unju/edm/model/Turista.java
+++ src/main/java/ar/edu/unju/edm/model/Turista.java
@@ -1,5 +1,13 @@
 package ar.edu.unju.edm.model;
 
 public class Turista {
+private int nombre;
 
+public int getNombre() {
+	return nombre;
+}
+
+public void setNombre(int nombre) {
+	this.nombre = nombre;
+}
 }
