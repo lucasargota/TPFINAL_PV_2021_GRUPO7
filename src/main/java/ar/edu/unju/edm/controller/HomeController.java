7dc9828f42823a54412b8d952e0bf744eea21bbc
Lucas Argota <lucas.argota287@gmail.com>
2021-06-10 13:16:51 -0300

    Agregado de websecurity
    y modificacion de clases
    Argota Lucas

--- src/main/java/ar/edu/unju/edm/controller/HomeController.java
+++ src/main/java/ar/edu/unju/edm/controller/HomeController.java
@@ -1,15 +1,16 @@
 package ar.edu.unju.edm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
 
-        @GetMapping("/home")
-        public String cargarInicio() {
-            return("home");
-        }
+	@GetMapping({"/","/login","/home", "/index","/login?error=true"})
+	public String cargarHome(Model model){	
+	return "home";
+	}
 
 
 }
