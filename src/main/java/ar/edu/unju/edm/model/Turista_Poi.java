238c4e8a9b39a720c8f1417f781599a9671e283c
Lucas Argota <lucas.argota287@gmail.com>
2021-06-15 18:49:07 -0300

    correccion de modificar y eliminar pois

--- src/main/java/ar/edu/unju/edm/model/Turista_Poi.java
+++ src/main/java/ar/edu/unju/edm/model/Turista_Poi.java
@@ -21,10 +21,13 @@ public class Turista_Poi {
 	private Integer idTurista_Poi;
 	
 	@Column
+	//@Max
+	//@Min
 	private int valoracion;
 	
 	@Column
-	private int comentario;
+	private String comentario;
+	
 	
 	@ManyToOne
 	@JoinColumn(name = "id")
@@ -54,13 +57,86 @@ public class Turista_Poi {
 		this.valoracion = valoracion;
 	}
 
-	public int getComentario() {
+	public String getComentario() {
 		return comentario;
 	}
 
-	public void setComentario(int comentario) {
+	public void setComentario(String comentario) {
 		this.comentario = comentario;
 	}
+
+	public Turista getOtroTurista() {
+		return otroTurista;
+	}
+
+	public void setOtroTurista(Turista otroTurista) {
+		this.otroTurista = otroTurista;
+	}
+
+	public Poi getOtroPoi() {
+		return otroPoi;
+	}
+
+	public void setOtroPoi(Poi otroPoi) {
+		this.otroPoi = otroPoi;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comentario == null) ? 0 : comentario.hashCode());
+		result = prime * result + ((idTurista_Poi == null) ? 0 : idTurista_Poi.hashCode());
+		result = prime * result + ((otroPoi == null) ? 0 : otroPoi.hashCode());
+		result = prime * result + ((otroTurista == null) ? 0 : otroTurista.hashCode());
+		result = prime * result + valoracion;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Turista_Poi other = (Turista_Poi) obj;
+		if (comentario == null) {
+			if (other.comentario != null)
+				return false;
+		} else if (!comentario.equals(other.comentario))
+			return false;
+		if (idTurista_Poi == null) {
+			if (other.idTurista_Poi != null)
+				return false;
+		} else if (!idTurista_Poi.equals(other.idTurista_Poi))
+			return false;
+		if (otroPoi == null) {
+			if (other.otroPoi != null)
+				return false;
+		} else if (!otroPoi.equals(other.otroPoi))
+			return false;
+		if (otroTurista == null) {
+			if (other.otroTurista != null)
+				return false;
+		} else if (!otroTurista.equals(other.otroTurista))
+			return false;
+		if (valoracion != other.valoracion)
+			return false;
+		return true;
+	}
+
+	public Turista_Poi(Integer idTurista_Poi, int valoracion, String comentario, Turista otroTurista, Poi otroPoi) {
+		super();
+		this.idTurista_Poi = idTurista_Poi;
+		this.valoracion = valoracion;
+		this.comentario = comentario;
+		this.otroTurista = otroTurista;
+		this.otroPoi = otroPoi;
+	}
+
+	
 	
 	
 }
