21d5790a7b0ddb7065bbabd971ce70857712290d
nanoo17 <marianojrodriguez10@gmail.com>
2021-07-01 21:00:20 -0300

    Configurando

--- src/main/java/ar/edu/unju/edm/WebSecurityConfig.java
+++ src/main/java/ar/edu/unju/edm/WebSecurityConfig.java
@@ -27,11 +27,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		 
 		 @Override
 			protected void configure(HttpSecurity http) throws Exception{
-				http
+			 http
+			//Correccion	
 				.requiresChannel()
 			      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
 			      .requiresSecure()
 			      .and()
+			      //ojo
 					.authorizeRequests()
 						.antMatchers(resources).permitAll()
 						.antMatchers("/","/home","/index","/turista/registrar","/turista/guardar").permitAll()
